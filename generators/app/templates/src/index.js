af48bbf35d3ae62bd58fbeb91c7ff65ddc9236bd
stan1812 <2314980309@qq.com>
2018-11-16 14:57:29 +0800

    add threejs

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -1,6 +1,9 @@
-import foo from './components/example.js'
+import foo from './components/example.js';
+import THREELib from 'three-js';
 import './assets/css/reset.css';
 import '../app.css';
 (function main() {
+  const THREE = THREELib(); // return THREE JS
+  console.log(THREE);
   foo();
 })();
